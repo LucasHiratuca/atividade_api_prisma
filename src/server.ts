4aa9f65802fd9ed6388b0d24665cab789cf2209c
LucasHiratuca <lucastetsuya2007@gmail.com>
2026-05-20 14:34:04 -0300

    aaaaaaaaaaaaaaaaaa

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import express from "express";
+import generosRouter from "./routes/generos";
+import jogosRouter from "./routes/jogos";
+import plataformasRouter from "./routes/plataformas";
 
-export const prisma = new PrismaClient();
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+    
+app.use("/generos", generosRouter);
+app.use("/jogos", jogosRouter);
+app.use("/plataformas", plataformasRouter);
+
+app.listen(PORT, () => {
+    console.log(`Servidor executando em localhost:${PORT}`)
+});
